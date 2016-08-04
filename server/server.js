be79f8a03e3de7e4896d18b248f0afcc006e761f
Shea Hawkins <Shea Hawkins>
2016-08-03 20:34:20 -0700

    Serve static assets

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
 var addRouter = require('./router');
+var path = require('path');
 
 var app = express();
 
 app.use(morgan('dev'));
 
 addRouter(app);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
+});
+
+app.use('/build', express.static(path.join(__dirname + '/../client/build')));
+app.use('/assets', express.static(path.join(__dirname + '/../client/assets')));
 
 app.listen(3000, function(err) {
   if (err) {throw err;};
