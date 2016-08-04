26a3c86273f5a5bbe0178e84867cb53fa6627d9f
Shea Hawkins <Shea Hawkins>
2016-08-03 22:07:13 -0700

    Connect to postgres container

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var addRouter = require('./router');
 var path = require('path');
+var connection = require('./db/db');
 
 var app = express();
 
