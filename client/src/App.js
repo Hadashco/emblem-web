ede7032f0cc3cac60431ba34273a160bd338f829
Conor C <Careyc@union.edu>
2016-08-04 17:02:24 -0700

    react skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(<Dashboard />, document.getElementById('app')); 
 
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return (
-         <div>Hello World</div>   
+        <div className='container'>
+            <div>Hello World</div>
+        </div>
         );
     }
 }
---
+++ client/src/resources/map/MapView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class MapView extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+}
