05fcb6b3512a295d7c32eb7be1a4c552e16c86d4
hchsquared <hannahc.henderson@gmail.com>
2016-08-06 16:30:25 -0700

    Remove config file

--- server/.config.js
+++
@@ -1,6 +0,0 @@
-// Create .config.js file with actual keys
-module.exports = {
-  SESSION_SECRET: 'session-secret',
-  FACEBOOK_ID: '317819878563445',
-  FACEBOOK_SECRET: '70f30d0aaf80feaafe29b20634a90ef2',
-};
