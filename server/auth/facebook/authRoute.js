a47510be557c2eaf6ac8ed4d9e5782d544f57f39
hchsquared <hannahc.henderson@gmail.com>
2016-08-06 11:24:35 -0700

    Add Facebook OAuth (wip)

---
+++ server/auth/facebook/authRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const config = require('../config')
+
+
+
+    clientID: process.env.FACEBOOK_ID || 'id',
+    clientSecret: process.env.FACEBOOK_SECRET || 'secret',
+    callbackURL: `${process.env.DOMAIN || ''}/auth/facebook/callback`,
---
+++ server/auth/facebook/passport.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+module.exports = (User, config) => {
+  const settings = {
+    clientId: config.facebook.clientID,
+    clientSecret: config.facebook.clientSecret,
+    callbackURL: config.facebook.callbackURL,
+    profileFields: ['id', 'displayName', 'link', 'photos', 'email'],
+  };
+
+  const handler = (accessToken, refreshToken, profile, done) => {
+    User.find({ where: { facebookId: profile.id } })
+      .then(user => {
+        if(user) {
+          return done(null, user);
+        }
+        const newUser = User.build({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          fbookId: profile.id,
+          imgUrl: profile.photos[0].value,
+          facebook: profile._json,
+        });
+      });
+      return newUser.save()
+        .then(createdUser => done(null, createdUser))
+        .catch(err => done(err));
+    })
+    .catch(err => done(err));
+  }
+}
