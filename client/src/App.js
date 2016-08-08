b8adc64ebcbbfc6633ec3966213b48dbb99f528e
hchsquared <hannahc.henderson@gmail.com>
2016-08-08 10:36:17 -0700

    Implement client-side Facebook OAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MapView from './resources/map/MapView';
-import Header from './resources/headerComponent.js'
-import {Provider} from 'react-redux';
-import {store} from './Store.js';
+import Header from './resources/headerComponent.js';
+import { Provider } from 'react-redux';
+import { store } from './Store.js';
 import Socket from './Socket';
 
 class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    addMarker(latLong) {
-        console.log(latLong);
-    }
+  addMarker(latLong) {
+    console.log(latLong);
+  }
 
-    render() {
-        return (
-        <div>
-            <Header />
-            <div className='mapUserContainer'>
-                <div className='container'>Hello World</div>
-                <MapView />
-                <button onClick={this.addMarker('marker added')}>Add a Marker!</button>
-            </div>
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="mapUserContainer">
+          <div className="container">Hello World</div>
+          <MapView />
+          <button onClick={this.addMarker('marker added')}>Add a Marker!</button>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
+
 ReactDOM.render(<Provider store={store}><Dashboard/></Provider>, document.querySelector('#app'));
