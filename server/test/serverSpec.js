14d11b007d5107075c2f4eb5d438e8e3f3575251
Shea Hawkins <Shea Hawkins>
2016-08-08 19:38:24 -0700

    Add tests for routes, fix other tests

--- server/test/serverSpec.js
+++ server/test/serverSpec.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect;
+const should = require('chai').should();
 const request = require('request');
 
 const connection = require('../db/db');
@@ -14,7 +14,7 @@ const userRecord = { fbookId: '0' };
 const xbeforeEach = () => {}; // Mimic xit and xdescribe
 
 describe('Build Models', () => {
- 
+
   beforeEach(() => {
     db.sync()
       .then(() => User.destroy({ where: userRecord }))
@@ -23,50 +23,106 @@ describe('Build Models', () => {
   });
 
   describe('User', () => {
-    it ('should build and save a new User', () => {
+    it ('should build and save a new User', (done) => {
       User.create(userRecord).then(user => {
         user.id.should.not.be.null;
         return User.findById(user.id).then(found => {
           found.id.should.equal(user.id);
+          done();
         });
       });
     });
   });
 
   describe('Place', () => {
-    it ('should build and save a new Place', () => {
+    it ('should build and save a new Place', (done) => {
       Place.create(placeRecord).then(place => {
         place.should.have.property('id');
         place.id.should.not.be.null;
         return Place.findById(place.id).then(found => {
           found.id.should.equal(place.id);
+          done();
         });
       });
     });
   });
 
   describe('Art', () => {
-    it ('should build and save a new Art', () => {
+    it ('should build and save a new Art', (done) => {
       Art.create(artRecord).then(art => {
         art.should.have.property('id');
         art.id.should.not.be.null;
         return Art.findById(art.id).then(found => {
           found.id.should.equal(art.id);
+          done();
         });
       });
     });
   });
-
-
 });
 
-/*
-  psql -h dbhost -U username dbname
-  psql -h localhost:5432 -U postgres postgres
-  dbhost: localhost:5432
-  dbname: postrgres
-  dbuser: postgres
-  pw: emblem
-  docker-compose up
-  psql -U postgres
-*/
+describe('Geotagging Routes', () => {
+  // for re-use throughout the tests
+  let place, art;
+
+  it ('should post a new Place to the server', (done) => {
+    new Promise((resolve, reject) => {
+      request.post({
+        url: 'http://localhost:3000/place',
+        body: JSON.stringify({long: 22.44, lat:88.00}),
+        headers: {
+          'content-type': 'application/json'
+        }
+      }, (err, response, body) => {
+        err ? reject(err) : resolve(response.body);
+      });
+    }).then(body => {
+        place = JSON.parse(body);
+        place.long.should.equal(22.44);
+        place.id.should.not.be.null;
+        done();
+      });
+  });
+
+
+
+  it ('should post a new art to the server', (done) => {
+    new Promise((resolve, reject) => {
+      request.post({
+        url: 'http://localhost:3000/art',
+        body: JSON.stringify({data: 'definitely an image'}),
+        headers: {
+          'file-type': 'img/fakeimg',
+          'content-type': 'application/octet-stream'
+        }
+      }, (err, response, body) => {
+        err ? reject(err) : resolve(response.body);
+      });
+    }).then(body => {
+      art = JSON.parse(body);
+      art.id.should.not.be.null;
+      done();
+    });
+  });
+
+
+
+  it ('should tie art to the place', (done) => {
+    new Promise((resolve, reject) => {
+      request.post({
+        url: `http://localhost:3000/place/${place.id}`,
+        body: JSON.stringify({id: art.id})
+      }, (err, response, body) => {
+        err ? reject(err) : resolve(response);
+      });
+    }).then(response => {
+      response.statusCode.should.equal(200);
+      done();
+    });
+  });
+
+  after(() => {
+    Place.findById(place.id).then(place => place.destroy());
+    Art.findById(art.id).then(art => art.destroy());
+  });
+});
