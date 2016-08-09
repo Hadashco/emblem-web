fcd21714c966d68960083cc43dde464ef8ac4fa3
hchsquared <hannahc.henderson@gmail.com>
2016-08-08 17:32:31 -0700

    Rebuild Facebook button as a React component

--- server/auth/facebook/facebookRoute.js
+++ server/auth/facebook/facebookRoute.js
@@ -3,12 +3,16 @@ const setTokenCookie = require('../authService').setTokenCookie;
 
 const router = require('express').Router();
 
+// TODO: Change success / failure redirects
+// Route to Facebook for authentication and login
 router.get('/', passport.authenticate('facebook', {
   scope: ['email', 'public profile'],
   failureRedirect: '/login',
+  successRedirect: 'http://localhost:3000/place',
   session: false,
 }));
 
+// Handle callback after Facebook user authentication
 router.get('/callback', passport.authenticate('facebook', {
   failureRedirect: '/login',
   session: false,
