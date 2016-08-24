af8a49e0d43cd3a41387d9eb0b8cc176aa18e392
hchsquared <hannahc.henderson@gmail.com>
2016-08-23 19:47:35 -0700

    Refactor artPlace routes to use controller

---
+++ server/resources/artPlace/artPlaceController.js
@@ -0,0 +1,157 @@
+const dbFile = require('../../db/db');
+const { ArtPlace, db } = dbFile;
+const Sequelize = require('sequelize');
+
+module.exports = {
+  // Get all ArtPlaces
+  getAll: (req, res) => {
+    ArtPlace.findAll().then(result => {
+      res.status(200).json(result);
+    });
+  },
+
+  // Get highest ArtPlace for each place
+  getMaxAtPlace: (req, res) => {
+    const qry = `SELECT DISTINCT ON ("ArtPlace"."PlaceId") "ArtPlace"."PlaceId", "User"."markerColor", 
+                        "Art"."UserId", "ArtPlace"."ArtId", "ArtPlace"."_id" AS "ArtPlaceId",
+                        ("ArtPlace".upvotes - "ArtPlace".downvotes) AS "netVotes", "Place".lat, "Place".long 
+                 FROM "Place" INNER JOIN  ("ArtPlace"  INNER JOIN 
+                        ("Art" INNER JOIN "User" ON "Art"."UserId" = "User".id) ON 
+                        "ArtPlace"."ArtId" = "Art".id) ON "ArtPlace"."PlaceId" = "Place".id 
+                 ORDER BY "ArtPlace"."PlaceId", ("ArtPlace".upvotes - "ArtPlace".downvotes) DESC`;
+    db.query(qry, { type: Sequelize.QueryTypes.SELECT })
+      .then(result => res.status(200).json(result))
+      .catch(err => {
+        console.log('artPlace/max/rank error:', err);
+        res.status(401).send(JSON.stringify(err));
+      });
+  },
+
+  // Get highest ArtPlace for each place between two bounds
+  getMaxBetweenBounds: (req, res) => {
+    const qry = `SELECT DISTINCT ON ("ArtPlace"."PlaceId") "ArtPlace"."PlaceId", 
+                        "User"."markerColor", "Art"."UserId", "ArtPlace"."ArtId", "Place".lat,
+                        ("ArtPlace".upvotes - "ArtPlace".downvotes) AS "netVotes", "Place".long,
+                        "ArtPlace"."_id" AS "ArtPlaceId" 
+                 FROM "Place" INNER JOIN  ("ArtPlace"  INNER JOIN 
+                        ("Art" INNER JOIN "User" ON "Art"."UserId" = "User".id) ON 
+                        "ArtPlace"."ArtId" = "Art".id) ON "ArtPlace"."PlaceId" = "Place".id 
+                 WHERE "Place"."lat" BETWEEN ${req.params.latMin} AND ${req.params.latMax} 
+                        AND "Place"."long" BETWEEN ${req.params.longMin} AND ${req.params.longMax}
+                 ORDER BY "ArtPlace"."PlaceId", ("ArtPlace".upvotes - "ArtPlace".downvotes) DESC`;
+    db.query(qry, { type: Sequelize.QueryTypes.SELECT })
+      .then(result => res.status(200).json(result))
+      .catch(err => {
+        console.log('artPlace/max/rank error:', err);
+        res.status(401).send(JSON.stringify(err));
+      });
+  },
+
+  // Get all ArtPlaces between two bounds
+  getAllBetweenBounds: (req, res) => {
+    const qry = `SELECT "ArtPlace"."PlaceId", "User"."markerColor", "Art"."UserId", "ArtPlace"."ArtId", 
+                        ("ArtPlace".upvotes - "ArtPlace".downvotes) AS "netVotes", 
+                        "Place".lat, "Place".long, "ArtPlace"."_id" AS "ArtPlaceId" 
+                 FROM "Place" INNER JOIN  ("ArtPlace"  INNER JOIN 
+                        ("Art" INNER JOIN "User" ON "Art"."UserId" = "User".id) ON 
+                        "ArtPlace"."ArtId" = "Art".id) ON "ArtPlace"."PlaceId" = "Place".id 
+                 WHERE "Place"."lat" BETWEEN ${req.params.latMin} AND ${req.params.latMax} 
+                        AND "Place"."long" BETWEEN ${req.params.longMin} AND ${req.params.longMax}
+                 ORDER BY ("ArtPlace".upvotes - "ArtPlace".downvotes) DESC`;
+    db.query(qry, { type: Sequelize.QueryTypes.SELECT })
+      .then(result => res.status(200).json(result))
+      .catch(err => {
+        console.log('artPlace/max/rank error:', err);
+        res.status(401).send(JSON.stringify(err));
+      });
+  },
+
+  // Get specific ArtPlace
+  getById: (req, res) => {
+    ArtPlace.findById(req.params.id).then(result => {
+      res.status(200).json(result);
+    });
+  },
+
+  // Add comment to ArtPlace
+  // Assumes that input includes: 1) artPlaceId 2) comment title
+  addCommentForId: (req, res) => {
+    ArtPlace.findById(req.params.id)
+      .then(artPlace => {
+        if (artPlace) {
+          artPlace.createComment({
+            title: req.body.title,
+          })
+          .then(comment => {
+            comment.setUser(req.user); // add creator ID
+            res.json(comment);
+          })
+          .catch(err => res.status(401).send(JSON.stringify(err)));
+        } else {
+          res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
+        }
+      });
+  },
+
+  // Get all comments for ArtPlace
+  getAllCommentsForId: (req, res) => {
+    ArtPlace.findById(req.params.id)
+      .then(artPlace => {
+        if (artPlace) {
+          artPlace.getComments()
+            .then(comments => {
+              res.status(200).json(comments);
+            })
+            .catch(err => res.status(401).send(JSON.stringify(err)));
+        } else {
+          res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
+        }
+      });
+  },
+
+  // Add vote, increment ArtPlace upvote / downvote
+  // Assumes that input includes: 1) artPlaceId in route 2) vote value (+1 or -1)
+  addVoteForId: (req, res) => {
+    let globalArtPlace;
+    ArtPlace.findById(req.params.id)
+      .then(artPlace => {
+        if (artPlace) {
+          globalArtPlace = artPlace;
+          artPlace.createVote({
+            value: req.body.vote,
+          })
+          .then(vote => {
+            vote.setUser(req.user); // add creator ID
+            if (req.body.vote > 0) globalArtPlace.increment('upvotes');
+            if (req.body.vote < 0) globalArtPlace.increment('downvotes');
+            res.status(200).json(vote);
+          })
+          .catch(err => res.status(401).send(JSON.stringify(err)));
+        } else {
+          res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
+        }
+      });
+  },
+
+  // Get votes for a specific ArtPlace ID
+  getVotesForId: (req, res) => {
+    ArtPlace.findById(req.params.id)
+      .then(artPlace => {
+        if (artPlace) {
+          artPlace.getVotes()
+            .then(votes => {
+              res.status(200).json(votes);
+            })
+            .catch(err => res.status(401).send(JSON.stringify(err)));
+        } else {
+          res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
+        }
+      });
+  },
+
+  deleteById: (req, res) => {
+    ArtPlace.destroy({ where: { _id: req.params.id } })
+      .then(() => res.status(200).send(`Successfully deleted ArtPlaceId ${req.params.id}`))
+      .catch(err => res.status(401).send(JSON.stringify(err)));
+  },
+};
--- server/resources/artPlace/artPlaceRoute.js
+++ server/resources/artPlace/artPlaceRoute.js
@@ -1,160 +1,38 @@
 const router = require('express').Router();
-const dbFile = require('../../db/db');
-const { ArtPlace, db } = dbFile;
-const Sequelize = require('sequelize');
+const { getAll, getMaxAtPlace, getMaxBetweenBounds,
+        getAllBetweenBounds, getById, addCommentForId,
+        getAllCommentsForId, addVoteForId, getVotesForId,
+        deleteById } = require('./artPlaceController');
 
 // Get all ArtPlaces
-router.get('/', (req, res) => {
-  ArtPlace.findAll().then(result => {
-    res.status(200).json(result);
-  });
-});
+router.get('/', getAll);
 
 // Get highest ArtPlace for each place
-router.get('/max/rank', (req, res) => {
-  const qry = `SELECT DISTINCT ON ("ArtPlace"."PlaceId") "ArtPlace"."PlaceId", "User"."markerColor", 
-                      "Art"."UserId", "ArtPlace"."ArtId", "ArtPlace"."_id" AS "ArtPlaceId",
-                      ("ArtPlace".upvotes - "ArtPlace".downvotes) AS "netVotes", "Place".lat, "Place".long 
-               FROM "Place" INNER JOIN  ("ArtPlace"  INNER JOIN 
-                      ("Art" INNER JOIN "User" ON "Art"."UserId" = "User".id) ON 
-                      "ArtPlace"."ArtId" = "Art".id) ON "ArtPlace"."PlaceId" = "Place".id 
-               ORDER BY "ArtPlace"."PlaceId", ("ArtPlace".upvotes - "ArtPlace".downvotes) DESC`;
-  db.query(qry, { type: Sequelize.QueryTypes.SELECT })
-    .then(result => res.status(200).json(result))
-    .catch(err => {
-      console.log('artPlace/max/rank error:', err);
-      res.status(401).send(JSON.stringify(err));
-    });
-});
-
+router.get('/max/rank', getMaxAtPlace);
 
 // Get highest ArtPlace for each place between two bounds
-router.get('/max/between/:latMin/:latMax/:longMin/:longMax', (req, res) => {
-  const qry = `SELECT DISTINCT ON ("ArtPlace"."PlaceId") "ArtPlace"."PlaceId", 
-                      "User"."markerColor", "Art"."UserId", "ArtPlace"."ArtId", "Place".lat,
-                      ("ArtPlace".upvotes - "ArtPlace".downvotes) AS "netVotes", "Place".long,
-                      "ArtPlace"."_id" AS "ArtPlaceId" 
-               FROM "Place" INNER JOIN  ("ArtPlace"  INNER JOIN 
-                      ("Art" INNER JOIN "User" ON "Art"."UserId" = "User".id) ON 
-                      "ArtPlace"."ArtId" = "Art".id) ON "ArtPlace"."PlaceId" = "Place".id 
-               WHERE "Place"."lat" BETWEEN ${req.params.latMin} AND ${req.params.latMax} 
-                      AND "Place"."long" BETWEEN ${req.params.longMin} AND ${req.params.longMax}
-               ORDER BY "ArtPlace"."PlaceId", ("ArtPlace".upvotes - "ArtPlace".downvotes) DESC`;
-  db.query(qry, { type: Sequelize.QueryTypes.SELECT })
-    .then(result => res.status(200).json(result))
-    .catch(err => {
-      console.log('artPlace/max/rank error:', err);
-      res.status(401).send(JSON.stringify(err));
-    });
-});
+router.get('/max/between/:latMin/:latMax/:longMin/:longMax', getMaxBetweenBounds);
 
 // Get all ArtPlaces between two bounds
-router.get('/between/:latMin/:latMax/:longMin/:longMax', (req, res) => {
-  const qry = `SELECT "ArtPlace"."PlaceId", "User"."markerColor", "Art"."UserId", "ArtPlace"."ArtId", 
-                      ("ArtPlace".upvotes - "ArtPlace".downvotes) AS "netVotes", 
-                      "Place".lat, "Place".long, "ArtPlace"."_id" AS "ArtPlaceId" 
-               FROM "Place" INNER JOIN  ("ArtPlace"  INNER JOIN 
-                      ("Art" INNER JOIN "User" ON "Art"."UserId" = "User".id) ON 
-                      "ArtPlace"."ArtId" = "Art".id) ON "ArtPlace"."PlaceId" = "Place".id 
-               WHERE "Place"."lat" BETWEEN ${req.params.latMin} AND ${req.params.latMax} 
-                      AND "Place"."long" BETWEEN ${req.params.longMin} AND ${req.params.longMax}
-               ORDER BY ("ArtPlace".upvotes - "ArtPlace".downvotes) DESC`;
-  db.query(qry, { type: Sequelize.QueryTypes.SELECT })
-    .then(result => res.status(200).json(result))
-    .catch(err => {
-      console.log('artPlace/max/rank error:', err);
-      res.status(401).send(JSON.stringify(err));
-    });
-});
-
+router.get('/between/:latMin/:latMax/:longMin/:longMax', getAllBetweenBounds);
 
 // Get specific ArtPlace
-router.get('/:id', (req, res) => {
-  ArtPlace.findById(req.params.id).then(result => {
-    res.status(200).json(result);
-  });
-});
+router.get('/:id', getById);
 
 // Add comment to ArtPlace
 // Assumes that input includes: 1) artPlaceId 2) comment title
-router.post('/:id/comment', (req, res) => {
-  ArtPlace.findById(req.params.id)
-    .then(artPlace => {
-      if (artPlace) {
-        artPlace.createComment({
-          title: req.body.title,
-        })
-        .then(comment => {
-          comment.setUser(req.user); // add creator ID
-          res.json(comment);
-        })
-        .catch(err => res.status(401).send(JSON.stringify(err)));
-      } else {
-        res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
-      }
-    });
-});
+router.post('/:id/comment', addCommentForId);
 
 // Get all comments for ArtPlace
-router.get('/:id/comment', (req, res) => {
-  ArtPlace.findById(req.params.id)
-    .then(artPlace => {
-      if (artPlace) {
-        artPlace.getComments()
-          .then(comments => {
-            res.status(200).json(comments);
-          })
-          .catch(err => res.status(401).send(JSON.stringify(err)));
-      } else {
-        res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
-      }
-    });
-});
+router.get('/:id/comment', getAllCommentsForId);
 
 // Add vote, increment ArtPlace upvote / downvote
 // Assumes that input includes: 1) artPlaceId in route 2) vote value (+1 or -1)
-router.post('/:id/vote', (req, res) => {
-  let globalArtPlace;
-  ArtPlace.findById(req.params.id)
-    .then(artPlace => {
-      if (artPlace) {
-        globalArtPlace = artPlace;
-        artPlace.createVote({
-          value: req.body.vote,
-        })
-        .then(vote => {
-          vote.setUser(req.user); // add creator ID
-          if (req.body.vote > 0) globalArtPlace.increment('upvotes');
-          if (req.body.vote < 0) globalArtPlace.increment('downvotes');
-          res.status(200).json(vote);
-        })
-        .catch(err => res.status(401).send(JSON.stringify(err)));
-      } else {
-        res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
-      }
-    });
-});
+router.post('/:id/vote', addVoteForId);
 
 // Get votes for a specific ArtPlace ID
-router.get('/:id/vote', (req, res) => {
-  ArtPlace.findById(req.params.id)
-    .then(artPlace => {
-      if (artPlace) {
-        artPlace.getVotes()
-          .then(votes => {
-            res.status(200).json(votes);
-          })
-          .catch(err => res.status(401).send(JSON.stringify(err)));
-      } else {
-        res.status(200).send(JSON.stringify(`No ArtPlace associated with id ${req.params.id}`));
-      }
-    });
-});
+router.get('/:id/vote', getVotesForId);
 
-router.post('/:id/delete', (req, res) => {
-  ArtPlace.destroy({ where: { _id: req.params.id } })
-    .then(() => res.status(200).send(`Successfully deleted ArtPlaceId ${req.params.id}`))
-    .catch(err => res.status(401).send(JSON.stringify(err)));
-});
+router.post('/:id/delete', deleteById);
 
 module.exports = router;
