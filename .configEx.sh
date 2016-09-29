a6f17d5e6baf6c63ac0c79fcbba8a5f0b3c5446c
Conor Carey <careyc@union.edu>
2016-09-28 23:30:01 -0400

    configEx put back in

---
+++ .configEx.sh
@@ -0,0 +1,9 @@
+# Create .config.sh file with actual keys and run in your console
+# bash .config.sh or npm start
+export SESSION_SECRET="ss-secret"
+export FACEBOOK_ID="fb-id"
+export FACEBOOK_SECRET="fb-secret"
+export AWS_ID="aws-access-key-id"
+export AWS_SECRET="aws-secret-access-key"
+export HOST_MODE="PROD"
+export HOST_SERVER="http://www.emblemar.com"
